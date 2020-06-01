44c1fbf1115f88ec0c9ca0e5adda0550a566ef0d
ValentinoT <danieltorresc13@gmail.com>
2020-06-01 07:36:26 -0500

    ep 22 reto completado

--- src/containers/ListOfPhotoCards.js
+++ src/containers/ListOfPhotoCards.js
@@ -1,4 +1,29 @@
-import { withPhotos } from '../hoc/withPhotos'
+import React from 'react'
+import { gql } from 'apollo-boost'
+import { Query } from 'react-apollo'
 import { ListOfPhotoCardsComponent } from '../components/ListOfPhotoCards'
 
-export const ListOfPhotoCards = withPhotos(ListOfPhotoCardsComponent)
+const query = gql`
+  query getPhotos($categoryId: ID){
+    photos(categoryId: $categoryId){
+      id
+      categoryId
+      src
+      likes
+      userId
+      liked
+    }
+  }
+`
+
+export const ListOfPhotoCards = ({ categoryId }) => (
+  <Query query={query}>
+    {
+      ({ loading, error, data }) => {
+        if (loading) { return null }
+        if (error) { console.log(error) }
+        return <ListOfPhotoCardsComponent data={data} />
+      }
+    }
+  </Query>
+)
