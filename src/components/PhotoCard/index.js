f2da1fde59446ca2c07dab6e7f2c97f0567549dd
ValentinoT <danieltorresc13@gmail.com>
2020-06-16 07:54:17 -0500

    Comprobar sesión para dar like

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,15 +1,12 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { ImgWrapper, Img, Article } from './styles'
-import { useLocalStorage } from '../../hooks/useLocalStorage'
 import { useNearScreen } from '../../hooks/useNearScreen'
 import { FavButton } from '../FavButton'
 import { ToggleLikeMutation } from '../../containers/ToggleLikeMutation'
 import { Link } from '@reach/router'
 const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
 
-export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
-  const key = `liked-${id}`
-  const [liked, setLiked] = useLocalStorage(key, false)
+export const PhotoCard = ({ id, liked, likes = 0, src = DEFAULT_IMAGE }) => {
   const [show, element] = useNearScreen()
 
   return (
@@ -26,8 +23,7 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
               {
                 (toggleLike) => {
                   const handleFavClick = () => {
-                    !liked && toggleLike({ variables: { input: { id } } })
-                    setLiked(!liked)
+                    toggleLike({ variables: { input: { id } } })
                   }
 
                   return (<FavButton liked={liked} likes={likes} onClick={handleFavClick} />)
