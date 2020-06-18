d348c035d348c811255922f94a1eb072ef2ebc4a
ValentinoT <danieltorresc13@gmail.com>
2020-06-18 07:54:54 -0500

    Redirecciones y rutas con reach-router

--- src/App.js
+++ src/App.js
@@ -1,43 +1,36 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Logo } from './components/Logo'
 import { GlobalStyle } from './styles/GlobalStyles'
 import { Home } from './pages/Home'
 import { Detail } from './pages/Detail'
 import { Favs } from './pages/Favs'
 import { User } from './pages/User'
+import { NotFound } from './pages/NotFound'
 import { NotRegisteredUser } from './pages/NotRegisteredUser'
-import { Router } from '@reach/router'
+import { Router, Redirect } from '@reach/router'
 import { Navbar } from './components/Navbar'
-import Context from './Context'
+import { Context } from './Context'
 
 export const App = () => {
   const urlParams = new window.URLSearchParams(window.location.search)
   const detailID = urlParams.get('detail')
-
+  const { isAuth } = useContext(Context)
   return (
     <>
       <Logo />
       <GlobalStyle />
       <Router>
+        <NotFound default />
         <Home path='/' />
         <Home path='/pet/:id' />
         <Detail path='/detail/:detailID' />
+        {!isAuth && <NotRegisteredUser path='/login' />}
+        {!isAuth && <Redirect noThrow from='/favs' to='/login' />}
+        {!isAuth && <Redirect noThrow from='/user' to='/login' />}
+        {isAuth && <Redirect noThrow from='/login' to='/' />}
+        <Favs path='/favs' />
+        <User path='/user' />
       </Router>
-      <Context.Consumer>
-        {
-          ({ isAuth }) =>
-            isAuth
-              ? <Router>
-                <Favs path='/favs' />
-                <User path='/user' />
-              </Router>
-              : <Router>
-                <NotRegisteredUser path='/favs' />
-                <NotRegisteredUser path='/user' />
-              </Router>
-        }
-      </Context.Consumer>
-
       <Navbar />
     </>
   )
