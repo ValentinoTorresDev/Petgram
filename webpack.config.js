ea11f26dd45bc24dd72a55fffbc6c9b2d35124f9
ValentinoT <danieltorresc13@gmail.com>
2020-06-23 07:48:15 -0500

    App convertida a una PWA

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin')
+const WebpackPwaManifestPlugin = require('webpack-pwa-manifest')
+const path = require('path')
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin')
+
 module.exports = {
   output: {
     filename: 'app.bundle.js',
@@ -7,6 +11,37 @@ module.exports = {
   plugins: [
     new HtmlWebPackPlugin({
       template: 'src/index.html'
+    }),
+    new WebpackPwaManifestPlugin({
+      name: 'Petgram - Tu app de fotos de mascotas',
+      shortname: 'Petgram',
+      description: 'Con Petgram puedes encontrar a tus mascotas favoritas',
+      background_color: '#fff',
+      theme_color: '#b1a',
+      icons: [
+        {
+          src: path.resolve('src/assets/icon.png'),
+          sizes: [96, 128, 192, 256, 384, 512]
+        }
+      ]
+    }),
+    new WorkboxWebpackPlugin.GenerateSW({
+      runtimeCaching: [
+        {
+          urlPattern: new RegExp('https://(res.cloudinary.com | images.unsplash.com)'),
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'images'
+          }
+        },
+        {
+          urlPattern: new RegExp('https://petgram-server.valentinot.now.sh'),
+          handler: 'NetworkFirst',
+          options: {
+            cacheName: 'Api'
+          }
+        }
+      ]
     })
   ],
   module: {
